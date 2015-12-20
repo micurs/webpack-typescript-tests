4f3a968082773c82070ea6da3124c8aab5c8b5b6
Michele Ursino <michele@micurs.com>
2015-12-20 13:39:01 -0800

    adding readme and add some comments in gulpfile.js

--- angular2_ts/gulpfile.js
+++ angular2_ts/gulpfile.js
@@ -2,22 +2,19 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var webpack = require('webpack-stream');
 
-gulp.task('connect', ['bundle'], function() {
-  connect.server({
-    root: ['./'],
-    port: 3000
-  });
-});
-
 gulp.task('bundle', function() {
 	return gulp.src('./src/app.ts')
-    .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest('./dist'))
-  ;
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('serve', ['bundle'], function() {
+  connect.server({ root: ['./'], port: 3000 });
 });
 
 gulp.task('build', ['bundle']);
 
-gulp.task('default', ['connect'], function() {
+// By default start the server and bundle the app in dist/bundle.js
+gulp.task('default', ['serve'], function() {
   gulp.watch('**/*.ts', ['bundle']);
 });
