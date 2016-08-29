0f2f7adace85ecce5a5b5babab01549d8264351b
Michele Ursino <michele@micurs.com>
2016-08-28 20:56:49 -0700

    Add optimization to skip dev code in React and other libraries.

--- multibundle/webpack.config.js
+++ multibundle/webpack.config.js
@@ -5,6 +5,7 @@ var path = require('path');
 var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 var UglifyJsPlugin = require("webpack/lib/optimize/UglifyJsPlugin");
 var SourceMapDevToolPlugin = require("webpack/lib/SourceMapDevToolPlugin");
+var DefinePlugin = require("webpack/lib/DefinePlugin");
 
 module.exports = {
   context: __dirname ,
@@ -29,9 +30,23 @@ module.exports = {
     ]
   },
   plugins: [
+    // Produce a common chunk with the shared code of all output chunks
     new CommonsChunkPlugin({
         name: "common",
+        // minChunks: 2,
+        // chunks: [ 'pagea', 'pageb', 'common' ]
     }),
-    new UglifyJsPlugin()
+    // Minify but suppress warnings
+    new UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    // Use the production node environment.
+    new DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
   ]
 }
